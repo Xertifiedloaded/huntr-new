a372a6d601c44c2c437442ad57dcb3de3e0af200
xertifiedloaded <certifiedloaded@yahoo.com>
2022-10-20 11:56:36 +0100

    my changes

--- assset/script/select.js
+++ assset/script/select.js
@@ -1,15 +1,37 @@
 let toggleLinkArr = document.querySelectorAll(".toggle-link")
-toggleLinkArr.forEach(toggleLink => {
-    console.log(toggleLink)
+console.log(toggleLinkArr)
+// forEach and for loop
+// toggleLinkArr.forEach(toggleLink => {
+//     console.log(toggleLink)
+//     let arrowDown = toggleLink.children[0]
+//     let toggleMenu = toggleLink.nextElementSibling
+//     arrowDown.addEventListener("click", showMobileMenu)
+//     toggleMenu.addEventListener("click", hideMobileMenu)
+//     function showMobileMenu() {
+//         toggleMenu.classList.remove("close") 
+//     }
+//     function hideMobileMenu() {
+//         toggleMenu.classList.add("close")
+
+//     }
+// })
+
+
+let isOnline = false
+for (i = 0; i < toggleLinkArr.length; i++) {
+    const toggleLink = toggleLinkArr[i]
     let arrowDown = toggleLink.children[0]
     let toggleMenu = toggleLink.nextElementSibling
     arrowDown.addEventListener("click", showMobileMenu)
-    toggleMenu.addEventListener("click", hideMobileMenu)
+    // toggleMenu.addEventListener("click", hideMobileMenu)
     function showMobileMenu() {
-        toggleMenu.classList.remove("close")
+        if (isOnline == false) {
+            isOnline = true
+            toggleMenu.classList.remove("close")
+            
+        } else {
+            isOnline = false
+            toggleMenu.classList.add("close")
+        }
     }
-    function hideMobileMenu() {
-        toggleMenu.classList.add("close")
-    }
-})
-
+}
